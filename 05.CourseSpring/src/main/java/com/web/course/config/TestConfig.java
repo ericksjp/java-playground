9bd7de5e0a1a33d018e70bde4319c42b5c4c5a73
ericksjp <ericksjp703@gmail.com>
2025-06-17 17:55:07 -0300

    feat(course): add Product endpoint

--- 05.CourseSpring/src/main/java/com/web/course/config/TestConfig.java
+++ 05.CourseSpring/src/main/java/com/web/course/config/TestConfig.java
@@ -11,9 +11,11 @@ import org.springframework.context.annotation.Profile;
 import com.web.course.entities.Category;
 import com.web.course.entities.Order;
 import com.web.course.entities.OrderStatus;
+import com.web.course.entities.Product;
 import com.web.course.entities.User;
 import com.web.course.repositories.CategoryRepository;
 import com.web.course.repositories.OrderRepository;
+import com.web.course.repositories.ProductRepository;
 import com.web.course.repositories.UserRepository;
 
 @Configuration
@@ -25,6 +27,8 @@ public class TestConfig implements CommandLineRunner {
     private OrderRepository orderRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -41,5 +45,21 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
         categoryRepository.saveAll(List.of(cat1, cat2, cat3));
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+        productRepository.saveAll(List.of(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(List.of(p1, p2, p3, p4, p5));
     }
 }
