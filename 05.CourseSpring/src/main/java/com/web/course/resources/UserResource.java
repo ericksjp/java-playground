d11594e08ed8fdec40099d93eee161cad6002864
ericksjp <ericksjp703@gmail.com>
2025-06-17 15:07:20 -0300

    feat(course): add user service

--- 05.CourseSpring/src/main/java/com/web/course/resources/UserResource.java
+++ 05.CourseSpring/src/main/java/com/web/course/resources/UserResource.java
@@ -1,18 +1,33 @@
 package com.web.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.course.entities.User;
+import com.web.course.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "erick", "erick@mail.com", "123423","alskdfjlsk");
-        return ResponseEntity.ok(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
     } 
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
 }
