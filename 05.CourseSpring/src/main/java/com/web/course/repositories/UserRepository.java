6dbf386f3939a4ac0d1f2776883650ef97611066
ericksjp <ericksjp703@gmail.com>
2025-06-17 14:09:31 -0300

    feat(course): add user jpa repository and seed db

---
+++ 05.CourseSpring/src/main/java/com/web/course/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.web.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.web.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
