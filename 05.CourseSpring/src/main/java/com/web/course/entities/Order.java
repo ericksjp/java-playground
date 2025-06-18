24717829174cf4a61bc0e05c8c9d731c7bea5901
ericksjp <ericksjp703@gmail.com>
2025-06-18 15:02:31 -0300

    feat(course): add Order-Product association with OrderItem association class

--- 05.CourseSpring/src/main/java/com/web/course/entities/Order.java
+++ 05.CourseSpring/src/main/java/com/web/course/entities/Order.java
@@ -2,8 +2,8 @@ package com.web.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,89 +11,99 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "orders")
 public class Order implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	private static final long serialVersionUID = 1L;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
-    private Instant moment;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	
+	private Integer orderStatus;
 
-    private Integer status;
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	public Order() {
+	}
 
-    public Order() {}
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.client = client;
+		setOrderStatus(orderStatus);
+	}
 
-    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
-        this.id = id;
-        this.moment = moment;
-        this.client = client;
-        this.status = orderStatus.getCode();
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Instant getMoment() {
+		return moment;
+	}
 
-    public Instant getMoment() {
-        return moment;
-    }
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
 
-    public void setMoment(Instant moment) {
-        this.moment = moment;
-    }
+	public User getClient() {
+		return client;
+	}
 
-    public User getClient() {
-        return client;
-    }
+	public void setClient(User client) {
+		this.client = client;
+	}
 
-    public void setClient(User client) {
-        this.client = client;
-    }
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
 
-    public void setStatus(OrderStatus status) {
-        if (status == null) return;
-        this.status = status.getCode();
-    }
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    public OrderStatus getStatus() {
-        return OrderStatus.valueOf(status);
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Order other = (Order) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
