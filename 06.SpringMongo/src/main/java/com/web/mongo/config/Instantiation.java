7ff80699c635b01783d216851ac74d9cba8d8e72
ericksjp <ericksjp703@gmail.com>
2025-06-20 14:49:47 -0300

    feat(smongo): add db seeding

---
+++ 06.SpringMongo/src/main/java/com/web/mongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.web.mongo.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.web.mongo.domain.User;
+import com.web.mongo.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(List.of(maria, alex, bob));
+    }
+}
