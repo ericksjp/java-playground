94b96085de50fe1dbd9b6ae1ebc0cb3c410493a4
ericksjp <ericksjp703@gmail.com>
2025-06-22 13:25:34 -0300

    refactor(smongo): create authorDTO

--- 06.SpringMongo/src/main/java/com/web/mongo/domain/Post.java
+++ 06.SpringMongo/src/main/java/com/web/mongo/domain/Post.java
@@ -7,6 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.web.mongo.dto.AuthorDTO;
 
 @Document(collection = "posts")
 public class Post implements Serializable {
@@ -17,11 +18,11 @@ public class Post implements Serializable {
     private Instant date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post() {}
 
-    public Post(String id, Instant date, String title, String body, User author) {
+    public Post(String id, Instant date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.title = title;
         this.body = body;
@@ -61,11 +62,11 @@ public class Post implements Serializable {
         this.date = date;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 }
