3ab7454c5a8f5d258322a4a75a423cd9d4493863
ericksjp <ericksjp703@gmail.com>
2025-06-22 13:25:34 -0300

    refactor(smongo): create authorDTO

--- 06.SpringMongo/src/main/java/com/web/mongo/config/Instantiation.java
+++ 06.SpringMongo/src/main/java/com/web/mongo/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.web.mongo.domain.Post;
 import com.web.mongo.domain.User;
+import com.web.mongo.dto.AuthorDTO;
 import com.web.mongo.repositories.PostRepository;
 import com.web.mongo.repositories.UserRepository;
 
@@ -32,8 +33,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(List.of(maria, alex, bob));
 
-        Post post1 = new Post(null, Instant.parse("2023-10-01T10:00:00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, Instant.parse("2024-10-02T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, Instant.parse("2023-10-01T10:00:00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, Instant.parse("2024-10-02T12:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(List.of(post1, post2));
     }
