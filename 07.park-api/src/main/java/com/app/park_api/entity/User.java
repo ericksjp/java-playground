2887b62c9fbba48a111eeae62469c6292ce0c909
ericksjp <ericksjp703@gmail.com>
2025-06-24 00:53:24 -0300

    feat(parkapi): add user entity

---
+++ 07.park-api/src/main/java/com/app/park_api/entity/User.java
@@ -0,0 +1,63 @@
+package com.app.park_api.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+@Entity(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(nullable = false, length = 200)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    @Column(name = "created_by", length = 100)
+    private String createdBy;
+    @Column(name = "updated_by", length = 100)
+    private String updatedBy;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENT
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User other = (User) o;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + "]";
+    }
+}
