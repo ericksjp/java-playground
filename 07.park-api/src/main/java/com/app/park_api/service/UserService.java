457f5c739d0001eafec9c8c02e95068cdf06a723
ericksjp <ericksjp703@gmail.com>
2025-06-24 13:34:10 -0300

    feat(parkapi): add endpoint get endpoint to list users

--- 07.park-api/src/main/java/com/app/park_api/service/UserService.java
+++ 07.park-api/src/main/java/com/app/park_api/service/UserService.java
@@ -1,5 +1,7 @@
 package com.app.park_api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +31,10 @@ public class UserService {
         User user = findById(id);
         user.setPassword(password);
         return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
