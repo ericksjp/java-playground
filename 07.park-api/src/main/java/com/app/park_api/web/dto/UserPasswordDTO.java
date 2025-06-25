2d1e284a63d2f3552cb8fded70dbf8eadc638df3
ericksjp <ericksjp703@gmail.com>
2025-06-25 00:33:49 -0300

    feat(parkapi): add some validations

--- 07.park-api/src/main/java/com/app/park_api/web/dto/UserPasswordDTO.java
+++ 07.park-api/src/main/java/com/app/park_api/web/dto/UserPasswordDTO.java
@@ -1,5 +1,7 @@
 package com.app.park_api.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,13 @@ import lombok.ToString;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UserPasswordDTO {
+    @NotBlank
+    @Size(min = 6, max = 6, message = "password must be 6 characters long")
     private String currentPassword;
+    @NotBlank
+    @Size(min = 6, max = 6, message = "password must be 6 characters long")
     private String newPassword;
+    @NotBlank
+    @Size(min = 6, max = 6, message = "password must be 6 characters long")
     private String confirmNewPassword;
 }
