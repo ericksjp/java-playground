74d4f7b7e9e3aa58ff98aca087630862c823c877
ericksjp <ericksjp703@gmail.com>
2025-06-26 20:40:48 -0300

    feat(parkapi): add SpringSecurityConfig

---
+++ 07.park-api/src/main/java/com/app/park_api/config/SpringSecurityConfig.java
@@ -0,0 +1,51 @@
+package com.app.park_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+// EnableMethodSecurity restrict access to specific service methods within the application
+
+@EnableMethodSecurity
+@Configuration
+public class SpringSecurityConfig {
+
+    // allows configuring web based security for specific http requests. By
+    // default it will be applied to all requests,
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            // disable some security options
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            // filter the acess per endpoint
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
+                .anyRequest().authenticated()
+            )
+            // set restfull policy
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    
+}
