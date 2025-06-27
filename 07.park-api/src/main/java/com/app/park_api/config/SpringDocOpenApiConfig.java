84c3002682a8e4c3c7dbce9dbd64ade83666e396
ericksjp <ericksjp703@gmail.com>
2025-06-27 19:08:57 -0300

    feat(parkapi): add swagger documentation for user controller

--- 07.park-api/src/main/java/com/app/park_api/config/SpringDocOpenApiConfig.java
+++ 07.park-api/src/main/java/com/app/park_api/config/SpringDocOpenApiConfig.java
@@ -3,10 +3,12 @@ package com.app.park_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SpringDocOpenApiConfig {
@@ -14,14 +16,24 @@ public class SpringDocOpenApiConfig {
     @Bean
     public OpenAPI openAPi() {
         return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
                 .info(
                     new Info()
-                    .title("REST API - Spring Park")
-                    .description("API for managing parking lots and users")
-                    .version("1.0.0")
-                    .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"))
+                        .title("REST API - Spring Park")
+                        .description("API for managing parking lots and users")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"))
                     .contact(new Contact().name("Erick Ribeiro").email("erick@mail.com"))
                 );
     }
-    
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .description("JWT Authorization header using the Bearer scheme. Example: 'Authorization: Bearer {token}'")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("security");
+    }
 }
