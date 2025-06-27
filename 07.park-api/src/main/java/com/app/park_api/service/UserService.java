c20a7ad23274486c82a653a07136feb0133b3040
ericksjp <ericksjp703@gmail.com>
2025-06-26 21:17:05 -0300

    feat(parkapi): update user Service and Repository classes
    - Add `findByUsername` method on both
    - Encrypt user password before saving in db in the user service

--- 07.park-api/src/main/java/com/app/park_api/service/UserService.java
+++ 07.park-api/src/main/java/com/app/park_api/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +19,13 @@ public class UserService {
 
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Transactional
     public User save(User user) {
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             return repository.save(user);
         } catch (DataIntegrityViolationException e) {
             throw new UsernameUniqueViolationException(String.format("username '%s' already in use", user.getUsername()));
@@ -46,11 +50,16 @@ public class UserService {
 
         User user = findById(id);
 
-        if (!user.getPassword().equals(currentPassword)) {
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
             throw new InvalidPasswordException("current password is incorrect");
         }
 
-        user.setPassword(newPassword);
+        user.setPassword(passwordEncoder.encode(newPassword));
         return user;
+    }
+
+    @Transactional(readOnly = true)
+    public User findByUsername(String username) {
+        return repository.findByUsername(username).orElseThrow(() -> new ResourceNotFoundException(String.format("user with username '%s' not found", username)));
     }
 }
