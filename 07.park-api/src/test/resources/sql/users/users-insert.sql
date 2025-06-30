063c383d98bf42d199cc624b96a3c70fa6a3d389
ericksjp <ericksjp703@gmail.com>
2025-06-30 15:15:08 -0300

    refactor(parkapi): update findUserById_WithRegisteredId_ReturnUserWithStatus200 test method

--- 07.park-api/src/test/resources/sql/users/users-insert.sql
+++ 07.park-api/src/test/resources/sql/users/users-insert.sql
@@ -1,3 +1,3 @@
-INSERT INTO USERS (id, username, password, role) VALUES (100, 'erick@mail.com', '123456', 'ROLE_ADMIN');
-INSERT INTO USERS (id, username, password, role) VALUES (101, 'maria@mail.com', '101010', 'ROLE_CLIENT');
-INSERT INTO USERS (id, username, password, role) VALUES (102, 'jorge@mail.com', 'fwe234', 'ROLE_CLIENT');
+INSERT INTO USERS (id, username, password, role) VALUES (100, 'erick@mail.com', '$2a$12$GsBtFjD29Mu6OL/twhQwx.aSwYSvWkKk9cYfshOKKCRo1cYUFcqA2', 'ROLE_ADMIN');
+INSERT INTO USERS (id, username, password, role) VALUES (101, 'maria@mail.com', '$2a$12$GsBtFjD29Mu6OL/twhQwx.aSwYSvWkKk9cYfshOKKCRo1cYUFcqA2', 'ROLE_CLIENT');
+INSERT INTO USERS (id, username, password, role) VALUES (102, 'jorge@mail.com', '$2a$12$GsBtFjD29Mu6OL/twhQwx.aSwYSvWkKk9cYfshOKKCRo1cYUFcqA2', 'ROLE_CLIENT');
