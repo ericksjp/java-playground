38de5d9cc6b066f2d990f06c12590c5ec196b483
ericksjp <ericksjp703@gmail.com>
2025-07-04 20:13:02 -0300

    feat(parkapi): add cors configuration

--- 07.park-api/src/main/java/com/app/park_api/config/SpringSecurityConfig.java
+++ 07.park-api/src/main/java/com/app/park_api/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,6 +42,7 @@ public class SpringSecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
+            .cors(Customizer.withDefaults())
             // filter the acess per endpoint
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
