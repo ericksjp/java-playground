f3835fa4acc99c8b2b08c9bb57139d0a93778811
ericksjp <ericksjp703@gmail.com>
2025-07-18 11:22:44 -0300

    feat(microsservices): add security to server

---
+++ 08.Microsservices/discovery-server/src/main/java/com/playground/server/config/SpringSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.playground.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .build();
+    }
+
+}
